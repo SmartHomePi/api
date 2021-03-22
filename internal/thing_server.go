e5d74732fd8afcf2ba527f798adce0b05225c799
Siddhesh <siddhesh.nandurkar@gmail.com>
2021-03-22 08:20:21 +0530

    initial commit

---
+++ internal/thing_server.go
@@ -0,0 +1,44 @@
+package thing
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		message := scanner.Text()
+		fmt.Println("Message Received:", message)
+		newMessage := strings.ToUpper(message)
+		conn.Write([]byte(newMessage + "\n"))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error:", err)
+	}
+}
+
+func StartThingServer() {
+	fmt.Println("Starting Thing Server...")
+
+	ln, err := net.Listen("tcp", "127.0.0.1:8081")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Accepting 'thing' TCP connections on port 8081")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Calling handleConnection")
+		go handleConnection(conn)
+	}
+}
